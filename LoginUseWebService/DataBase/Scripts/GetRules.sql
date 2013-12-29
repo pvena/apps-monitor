e3a6684020ee792693781f0e6612708033799494
pablopvnet <pablopvnet@gmail.com>
2013-12-29 05:52:41 +0000

    Agrego a la base de datos la estructura de tablas para manajar reglas y condiciones.

---
+++ LoginUseWebService/DataBase/Scripts/GetRules.sql
@@ -0,0 +1,29 @@
+if(exists (select * from sys.objects where type like 'p' and 
+											name like 'getRules'))
+	drop procedure getRules
+go 
+--getRules '666d4bbefa5393d5'
+create procedure getRules
+@phoneId nvarchar(20)
+
+as
+declare @idUser int 
+
+select top 1 @idUser = idUser from [user] u where u.phoneId like @phoneId or u.name like @phoneId
+
+select 
+	r.idRule,
+	r.commandKey,
+	t.name +'-'+p.name as [key],
+	pv.value
+from [rule] r
+inner join condition c  on
+	c.idRule = r.idRule
+inner join propertyValue pv on
+	pv.idPropertyValue = c.idPropertyvalue
+inner join [property] p on 
+	pv.idProperty = p.idProperty
+inner join [type] t on
+	p.idType = t.idType
+where r.idUser = @idUser
+order by r.idRule
