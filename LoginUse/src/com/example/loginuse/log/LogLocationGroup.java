cd5b1321454b26a8413b4a98e811e84c3954c1fb
pablopvnet <pablopvnet@gmail.com>
2013-12-31 19:23:49 +0000

    la que faltaba

---
+++ LoginUse/src/com/example/loginuse/log/LogLocationGroup.java
@@ -0,0 +1,39 @@
+package com.example.loginuse.log;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class LogLocationGroup {
+	private float longitud;
+	private float latitud;
+	private int count;
+	private String name;
+	
+	public void setName(String value){
+		this.name = value;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public void setLongitud(float value){
+		this.longitud = value;
+	}
+	public float getLongitud(){
+		return this.longitud;
+	}
+	public void setLatitud(float value){
+		this.latitud = value;
+	}
+	public float getLatitud(){
+		return this.latitud;
+	}
+	public void setCount(int value){
+		this.count = value;
+	}
+	public int getCount(){
+		return this.count;
+	}
+	
+	public LatLng getLatLng(){
+		return new LatLng(this.latitud, this.longitud);
+	}
+}
